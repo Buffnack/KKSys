9598abbf50b982eec9c022e9c5727f311456ba6c
Lennart Nehlsen <finnlennart.nehlsen@student.uni-luebeck.de>
2018-08-10 23:26:00 +0200

    extra commit

---
+++ KKSysForms/KKSysForms/Run.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KKSysForms
+{
+    class Run
+    {
+        public Run()
+        {
+
+        }
+
+        //maybe list of bytes[]
+        public void handleDB()
+        {
+
+        }
+    }
+}
