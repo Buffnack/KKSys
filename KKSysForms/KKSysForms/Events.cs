0c343c1142772a87b3c5cd614b117abd7211d1af
Lennart Nehlsen <finnlennart.nehlsen@student.uni-luebeck.de>
2018-08-15 22:56:08 +0200

    Heavy Changes in Event

--- KKSysForms/KKSysForms/Events.cs
+++ KKSysForms/KKSysForms/Events.cs
@@ -1,16 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 
-//Serializeability is missing
 //Std value for start end
+//TODO: ANstehende Events sollen eine View bekommen in der Database
 namespace KKSysForms_Event
 {
     internal enum DayCode { Mon, Di, Mi, Do, Fr, Sa, So }
-    [Serializable]
+
+    class TimeStamp
+    {
+        private int h { get; }
+
+        private int m { get; }
+
+        public TimeStamp(int h, int m)
+        {
+            if (h < 0 || h > 23 || m < 0 || m > 59)
+            {
+                throw new Exception("Das sind keine gueltigen Werte");
+            }
+            else
+            {
+                this.h = h;
+                this.m = m;
+            }
+        }
+
+        public void generateTime(TimeStamp time, int durationInMinutes)
+        {
+
+        }
+
+    }
+
+
+    //Enthaelt Information ueber das Label mehrere Veranstaltungen
     class EventLabel
     {
         public String Name { get; }
+
         private List<Event> eventsUnderLabel;
         
 
@@ -21,14 +51,14 @@ namespace KKSysForms_Event
             
         }
 
-        public void addEvent()
+        public void addEvent(Event @event)
         {
-
+            this.eventsUnderLabel.Add(@event);
         }
 
-        public void removeEvent()
+        public void removeEvent(Event @event)
         {
-
+            this.eventsUnderLabel.Remove(@event);
         }
 
         public List<Event> getEventList()
@@ -38,50 +68,56 @@ namespace KKSysForms_Event
 
         
     }
-    
-    abstract class Event
+    [Serializable]
+    abstract class Event : ISerializable
     {
+
         public int Start { get; set; }
 
         public int End { get; set; }
 
         public String Name { get; set; }
 
+        [NonSerialized]
         public EventLabel EventLabel;
 
+        [NonSerialized]
         protected bool modified;
 
+        [NonSerialized]
         protected bool created;
 
-        public Event(EventLabel label,String name, int start, int end)
+        //Constructor for Creation
+        public Event(EventLabel label, String name, int start, int end)
         {
-            
+
             this.EventLabel = label;
+            this.Name = name;
+            this.Start = start;
+            this.End = end;
+            this.created = true;
+        }
+        //For Deserialisation
+        public Event(String name, int start, int end)
+        {
             this.Name = name;
             this.Start = start;
             this.End = end;
         }
 
-
-    }
-
-  
-
-    abstract class RepeatingEvents : Event
-    {
-        protected DayOfWeek dayCode;
-
-        public RepeatingEvents(EventLabel lab,String name, int start, int end, DayOfWeek dayCode) : base(lab,name, start, end)
+        public void setModified()
         {
-
+            if (created)
+            {
+                return;
+            }
+            else
+            {
+                this.modified = true;
+            }
         }
 
-        public void createUniqueChangeEvent()
-        {
-
-        }
-
-
+        public abstract void GetObjectData(SerializationInfo info, StreamingContext context);
 
     }
 
@@ -89,14 +125,16 @@ namespace KKSysForms_Event
 
     abstract class NonRepeatingEvents : Event
     {
-        protected RepeatingEvents replace { get; set; }
+        
         protected int numOfReplace { get; set; }
 
-        public NonRepeatingEvents(RepeatingEvents forEvent) : base(forEvent.EventLabel,forEvent.Name, forEvent.Start, forEvent.End)
+        //With Reference
+        public NonRepeatingEvents(EventLabel lab, String name, int start, int end, DateTime date) : base(lab, name, start, end)
         {
 
         }
-        public NonRepeatingEvents(EventLabel lab, String name, int start, int end, DateTime date) : base(lab, name, start, end)
+
+        public NonRepeatingEvents(String name, int start, int end, DateTime date) : base(name, start, end)
         {
 
         }
@@ -105,12 +143,20 @@ namespace KKSysForms_Event
     //Fuer Termine, die mal verschoben werden (mit Ausfall)
     class ReferencedOneTimeEvent : NonRepeatingEvents
     {
-        public ReferencedOneTimeEvent(RepeatingEvents repeatingEvent) : base(repeatingEvent)
+        //TODO: Datatype
+        private Event reference;
+
+        public ReferencedOneTimeEvent(Event reference, int start, int end, DateTime date) : base(reference.EventLabel, reference.Name, start, end, date)
         {
 
         }
 
         
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
     //Fuer Termine, welche nicht einen Termin verschieben (wie KLausuren, Extra Vorlesung etc)
     class NonReferencedOneTimeEvent : NonRepeatingEvents
@@ -120,37 +166,33 @@ namespace KKSysForms_Event
 
         }
 
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 
 
-    class Lecture : RepeatingEvents
+    class Lecture : Event
     {
-
+        public DayOfWeek dayCode;
+        private int Start; //1030
+        // Nutzer: Start = 10, dauer 45min --> Ende wurde nicht angegeben, aber es ist eine Lecture, -> Ende = Start ^Dauer
         private String location { get; set; }
         private String additionalInformation { get; set; }
 
-        public Lecture(EventLabel lab,String name, int start, int end, DayOfWeek dayCode, String location, String additonalInformation) : base(lab,name, start, end, dayCode)
+        public Lecture(EventLabel lab,String name, int start, int end, DayOfWeek dayCode, String location, String additonalInformation) : base(lab,name, start, end)
         {
 
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
         }
     }
 
-    class Task : RepeatingEvents
-    {
-        private DateTime deadLine;
-
-        
-        public Task(EventLabel lab,String name, int start, int end, DayOfWeek dayCode) : base (lab,name, start, end, dayCode)
-        {
-            
-        }
-
-        private void generateDeadLine()
-        {
-            //how?
-        }
-
-    }
+   
 
     
 
--- KKSysForms/KKSysForms/InitializeDatabase.sql
+++ KKSysForms/KKSysForms/InitializeDatabase.sql
@@ -1,6 +1,7 @@
-﻿create Table EventArt IF NOT EXISTS ( ID INTEGER PRIMARY KEY ASC, Discription varchar not null);
-create Table KarteiArt IF NOT EXISTS( ID INTEGER PRIMARY KEY ASC, typus varchar not null);
+﻿create Table KarteiArt IF NOT EXISTS( ID INTEGER PRIMARY KEY ASC, typus varchar not null);
 create Table EventLabel IF NOT EXISTS(ID INTEGER PRIMARY KEY ASC, nameOf varchar not null);
-create Table EventsTable IF NOT EXISTS(ID INTEGER PRIMARY KEY ASC, belongTo INTEGER, timeStart INTEGER, timeEnd INTEGER, nameOf varchar, dayCode Integer not null, FOREIGN KEY(belongTo) REFERENCES EventLabel(ID));
+create Table RepeatEvents IF NOT EXISTS(ID INTEGER PRIMARY KEY AUTOINCREMENT, LabelID INTEGER NOT NULL, NameOf varchar,serialized BLOB, DayCode varchar not null, FOREIGN KEY(LabelID) REFERENCES EventLabel(ID));
+create Table UniqueEvents IF NOT EXISTS(ID INTEGER PRIMARY KEY AUTOINCREMENT, LabelID INTEGER NOT NULL, NameOf varchar, serialized BLOB, day date not null, FOREIGN KEY(LabelID) REFERENCES EventLabel(ID));
+create Table ReplaceEvents IF NOT EXISTS(ID INTEGER PRIMARY KEY AUTOINCREMENT, RepeatEvent INTEGER NOT NULL, NameOf varchar, serialized BLOB, Date varchar not null, FOREIGN KEY(RepeatEvent) REFERENCES RepeatEvents(ID));
 create Table KarteiCard IF NOT EXISTS (ID INTEGER PRIMARY KEY ASC, serialized blob, kindOF INTEGER, belongTo INTEGER, tagID int, FOREIGN KEY(kindOf) REFERENCES KarteiArt(ID), FOREIGN KEY (belongTo) REFERENCES EventLabel(ID), FOREIGN KEY (tagID) REFERENCES Tag(ID));
 create Table Tag IF NOT EXISTS (ID INTEGER PRIMARY KEY ASC, nameOf varchar, kurz varchar, code blob)
